8763a9ed99aa42fecb663b95df4e7802c5724035
Ashley Coles <ashley@acoles.co.uk>
2024-03-19 14:56:30 +0000

    Models and factories for part one

---
+++ app/Models/Contract.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Contract extends Model
+{
+    use HasFactory;
+
+    public function Employees(): HasMany
+    {
+        return $this->hasMany(Employee::class);
+    }
+}
