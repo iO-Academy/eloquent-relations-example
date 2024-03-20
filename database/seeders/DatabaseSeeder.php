3a6c6d194fc598f1965ed7de5b1e376fc7cc92c2
Ashley Coles <ashley@acoles.co.uk>
2024-03-20 08:44:09 +0000

    Fixing seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             ContractSeeder::class,
             EmployeeSeeder::class,
-            CertificationSeeder::class
+            CertificationSeeder::class,
+            CertificationEmployeeSeeder::class
         ]);
     }
 }
